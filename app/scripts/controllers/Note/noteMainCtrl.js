130246ff5e1b42cd4a3e2c0aeaa6c81350c7b932
Yixi <lyh1112@gmail.com>
2013-11-11 23:05:52 +0800

    ignore

--- app/scripts/controllers/Note/noteMainCtrl.js
+++ app/scripts/controllers/Note/noteMainCtrl.js
@@ -4,11 +4,15 @@
 
 angular.module('diigoApp').
     controller('noteMainCtrl',function($scope,ItemsModel){
-        $scope.$on('item.update',function(event){
-            $scope.items = ItemsModel.items;
-            console.log($scope);
-            console.log($scope.items.length);
-        });
-        console.log($scope);
+//        $scope.$on('item.update',function(event){
+////            $scope.items = ItemsModel.items;
+////            console.log($scope);
+////            console.log($scope.items.length);
+//            $scope.items.push({title:"d123",content:"123"});
+//        });
+//        console.log($scope);
         $scope.items = ItemsModel.items
+        $scope.addItem = function(){
+            ItemsModel.addItem({title:"dasf",content:"123123"});
+        }
     });
